dc51a63c7b625715c8f09cb9fa1aa59e8aa82b3d
NjeriNjoroge <wanjorogegrace@gmail.com>
2019-03-10 12:07:17 +0300

    Initial Commit

---
+++ AloeStackViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AloeStackViewTutorial
+//
+//  Created by Grace Njoroge on 10/03/2019.
+//  Copyright © 2019 Grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
